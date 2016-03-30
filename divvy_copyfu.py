5d8cff94cdd850938ff6c978bb02b39dcb28f520
Scott Pham <scpham@cisco.com>
2016-03-30 11:26:11 -0400

    Cleaned up and added progress bar

--- divvy_copyfu.py
+++ divvy_copyfu.py
@@ -18,24 +18,32 @@ import getpass
 import os
 import sys
 import threading
+import Queue
 from subprocess import Popen, PIPE
 from time import sleep
-
+import click
 import paramiko
+import logging
 
 
 class CopyFu(object):
     def __init__(self, streams=10,**kwargs) :
         """Object Init"""
 
+        self.progress_queue = Queue.Queue()
         self.filename = kwargs.get('filename')
         self.streams = streams
         self.username = kwargs.get('username')
-        #self.password = kwargs.get('password')
         self.hostname = kwargs.get('hostname')
         self.dest_file = kwargs.get('destination_file')
         self.ssh_key = kwargs.get('ssh_key')
         self.dev_null = open(os.devnull, 'w')
+        log_format = '%(filename)s: %(levelname)s: %(funcName)s(): %(lineno)d:\t %(message)s'
+        logging.basicConfig(format=log_format)
+        self.logger   = logging.getLogger('CopyFu')
+        self.logger.setLevel(logging.INFO)
+        if kwargs.get('debug'):
+            self.logger.setLevel(logging.DEBUG)
 
         self.ssh_args = dict(
             hostname=self.hostname,
@@ -43,37 +51,31 @@ class CopyFu(object):
             allow_agent=True,
             look_for_keys=True
         )
+
         if self.ssh_key:
             self.ssh_args['key_filename'] = self.ssh_key
 
         if kwargs.get('get_password'):
             self.password = getpass.getpass()
             self.ssh_args['password'] = self.password
-            self.ssh_args['allow_agent']=False
-            self.ssh_args['look_for_keys']=False
+            self.ssh_args['allow_agent']   = False
+            self.ssh_args['look_for_keys'] = False
         else:
             self.password = None
         #self.stoprequest = threading.Event()
 
     def _generate_queue_items(self):
-        """"""
-        self.file_size = os.path.getsize(self.filename)
-        print "File Size: %s" % self.file_size
-        self.total_blocks = self.file_size / 4096
-        print "Total Blocks: %s" % self.total_blocks
+        """ Generates List of Offsets """
 
-        # We need to check if the remaining blocks are 0
-        # If it's a zero we need to ensure that we have blocks remaining or the file will be corrupt.
-       # while True:
-       #     remainder = self.total_blocks % self.streams
-       #     if remainder == 0:
-       #         self.streams += 1
-       #         continue
-       #     break
+        self.file_size = os.path.getsize(self.filename)
+        self.logger.debug("File Size: %s" % self.file_size)
+        self.total_blocks = self.file_size / 4096
+        self.logger.debug("Total Blocks: %s" % self.total_blocks)
+
         self.blocks_per_stream = self.total_blocks / self.streams
-        print "Streams: %s" % self.streams
+        self.logger.debug("Streams: %s" % self.streams)
         self.blocks_remainder = self.total_blocks % self.streams
-        print "Left Over Blocks: %s" % self.blocks_remainder
+        self.logger.debug("Left Over Blocks: %s" % self.blocks_remainder)
 
         offset = 0
         self.queue=[]
@@ -85,8 +87,8 @@ class CopyFu(object):
     def start_threads(self):
         """ Create and Start Threads for Copying """
 
-
-        #pool = [threading.Thread(name="Thread-%s" % i, target=self.do_copy) for i in range(self.streams)]
+        # Generate Queue Items
+        self._generate_queue_items()
 
         pool = []
         last_item = self.queue[-1]
@@ -96,10 +98,25 @@ class CopyFu(object):
             else:
                 pool.append(threading.Thread(target=self.copy, args=(item,self.blocks_per_stream)))
 
-        for thread in pool:
-            thread.start()
-            # Need to throttle the ssh connections
-            sleep(1)
+        with click.progressbar(length=len(self.queue*2),label="Copy Progress") as bar:
+
+            for thread in pool:
+                thread.start()
+                # Need to throttle the ssh connections
+                sleep(1)
+                bar.next()
+
+            max_items = len(self.queue)
+            while True:
+                try:
+                    if max_items == 0:
+                        break
+                    blocks = self.progress_queue.get(True,0.05)
+                    bar.next()
+                    max_items -= 1
+                except Queue.Empty:
+                    continue
+
 
 
     def copy(self,offset, block_count):
@@ -112,7 +129,7 @@ class CopyFu(object):
 
 
         thread_name = threading.currentThread().getName()
-        print "Copying Offset: %s Block Count %s on Thread %s" % (offset, block_count, thread_name)
+        self.logger.debug("Copying Offset: %s Block Count %s on Thread %s" % (offset, block_count, thread_name))
 
         #read_cmd = "dd if=%s bs=4096 skip=%s count=%s | gzip -c | ssh 10.203.96.10 'gzip -dc | dd conv=notrunc of=%s bs=4096 seek=%s'" % (self.filename, offset, self.blocks_per_stream, self.dest_file, offset)
         #(cmdin,cmdout,cmderr) = os.popen3(read_cmd, 'r')
@@ -136,14 +153,15 @@ class CopyFu(object):
 
         ssh_channel.setblocking(0)
         ssh_channel.exec_command(write_dd_cmd)
-        print "Sending Thread %s %s bytes" % (thread_name,sys.getsizeof(gzip_stdout))
+        self.logger.debug("Sending Thread %s %s bytes" % (thread_name,sys.getsizeof(gzip_stdout)))
         ssh_channel.sendall("%s" %gzip_stdout)
         # Need to call shutdown(2) and recv_exit_status for this to flush all data sent
         ssh_channel.shutdown(2)
-        print "Thread %s Exit Status %s" % (thread_name,ssh_channel.recv_exit_status())
+        self.logger.debug("Thread %s Exit Status %s" % (thread_name,ssh_channel.recv_exit_status()))
 
         ssh_client.close()
 
+        self.progress_queue.put(block_count)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -154,6 +172,7 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--key', dest='ssh_key', type=str,help="Supply SSH Key")
     parser.add_argument('-u', '--username', dest='username',type=str, required=True, help="Provide User name")
     parser.add_argument('-s','--streams',dest='streams', type=int, help="How Many SSH Copies jobs to start")
+    parser.add_argument('--debug', dest='debug', action='store_true', help="Show Debug")
 
     args = parser.parse_args()
 
@@ -163,9 +182,7 @@ if __name__ == '__main__':
         if value is not None:
             copyfu_args[arg] = value
 
-    print copyfu_args
 
     #copyfu = CopyFu(filename=args.filename, destination_file=args.destination_file)
     copyfu = CopyFu(**copyfu_args)
-    copyfu._generate_queue_items()
     copyfu.start_threads()
